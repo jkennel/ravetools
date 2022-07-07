ac21b5ed021b3800f42f44b608198a6bf2b6bcbd
Jonathan Kennel <jonathankennel@Jonathans-MacBook-Air.local>
2022-07-07 06:12:07 -0700

    match c++ function calls

--- src/fftw-wrapper.h
+++ src/fftw-wrapper.h
@@ -3,12 +3,15 @@
 
 #include <Rcpp.h>
 
-SEXP fftw_r2c(SEXP data, int HermConj, SEXP ret);
+SEXP fftw_r2c(SEXP data, int HermConj, int fftwplanopt, SEXP ret, bool inplace);
 
-SEXP mvfft_r2c(SEXP data, int HermConj, int fftwplanopt, SEXP ret);
+SEXP mvfft_r2c(SEXP data, int HermConj, int fftwplanopt, SEXP ret, bool inplace);
 
-SEXP fftw_c2c(SEXP data, int inverse, SEXP ret);
+SEXP fftw_c2c(SEXP data, int inverse, SEXP ret, bool inplace);
+
+SEXP fftw_c2r(SEXP data, int HermConj, SEXP ret, bool inplace);
 
 SEXP conjugate(SEXP data);
 
 #endif // RAVEUTILS_FFTW_WRAPPER_H
+
